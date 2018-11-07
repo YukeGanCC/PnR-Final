393ef7d571dc8d1a883ecdbd8e0d3989f4a37c81
gany <gany@gilmour.org>
2018-11-07 14:37:14 -0500

    change self.direction_choice method.

--- student.py
+++ student.py
@@ -252,7 +252,7 @@ class Piggy(pigo.Pigo):
                 # add up the numbers to left1_total
                 m['left1_total'] += self.scan[angle]
 
-        if m['left1_total'] + m['right1_total'] + m['right2_total'] + m['left2_total'] < 1000:
+        if m['left1_total'] + m['right1_total'] + m['right2_total'] + m['left2_total'] < 500:
             self.encL(1)
         #  if left1 is bigger:
         elif max(m, key=m.get) == 'left1_total':
