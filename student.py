ac5c9b0ba6b9eac3b409becdba199b7b27b4b953
gany <gany@gilmour.org>
2018-11-19 14:56:13 -0500

    change hard stop distance

--- student.py
+++ student.py
@@ -20,7 +20,7 @@ class Piggy(pigo.Pigo):
         self.MIDPOINT = 75
         # YOU DECIDE: How close can an object get (cm) before we have to stop?
         self.SAFE_STOP_DIST = 40
-        self.HARD_STOP_DIST = 15
+        self.HARD_STOP_DIST = 30
         # YOU DECIDE: What left motor power helps straighten your fwd()?
         self.LEFT_SPEED = 125
         # YOU DECIDE: What left motor power helps straighten your fwd()?
